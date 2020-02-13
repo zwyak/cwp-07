b75d47b945d6d35a09300c9d60bafcf0bc6f0752
zwyak <vlad.senyuk97@gmail.com>
2020-02-13 14:58:39 +0300

    Refactored project files

--- public/form.js
+++ public/form.js
@@ -1,13 +1,10 @@
 $('#submit').click(function () {
-
-    alert('Request was sent');
-
     $.ajax({
         type: "post",
         url: "/api/articles/create",
         data: JSON.stringify({title: $("#title").val(), text: $( "#text" ).val(), author: $("#author").val() }),
         success: function (data) {
-          alert('Request was sent');
+          alert('Success!');
         }
       });
 });
